3e143cbf4f47f39e34f21f5e2368479dcbc1ef1e
Bhanu2112 <bodasingibhanuprasad@gmail.com>
2024-08-04 02:18:08 +0530

    begining of projects setup

---
+++ services/group-service/src/test/java/com/org/bhanu/groupservice/GroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.bhanu.groupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
